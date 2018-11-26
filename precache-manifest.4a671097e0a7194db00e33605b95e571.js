b4b793728b302c062b991b0a7ba6e52be992e95a
unknown <zbignevus@gmail.com>
2018-11-26 20:43:08 +0200

    Updates

---
+++ precache-manifest.4a671097e0a7194db00e33605b95e571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b62b8ff23eb9cd45dc8d",
+    "url": "/placard3/static/js/runtime~main.b62b8ff2.js"
+  },
+  {
+    "revision": "810bf711209ee158914a",
+    "url": "/placard3/static/js/main.810bf711.chunk.js"
+  },
+  {
+    "revision": "ffb28e0a032f494bed98",
+    "url": "/placard3/static/js/1.ffb28e0a.chunk.js"
+  },
+  {
+    "revision": "810bf711209ee158914a",
+    "url": "/placard3/static/css/main.e9519c84.chunk.css"
+  },
+  {
+    "revision": "4bf104e430c3ebcbecfad08792da7a83",
+    "url": "/placard3/index.html"
+  }
+];
